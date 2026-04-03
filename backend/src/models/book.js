b3a67c74238a6a0ef9240974e020a365d297fb8c
Doniyor <ddonierov95@gmail.com>
2026-04-03 15:18:05 +0500

    backend: change book db and correct the api insha'Allah in future

--- backend/src/models/book.js
+++ backend/src/models/book.js
@@ -3,8 +3,9 @@ import { model, Schema } from "mongoose";
 const BookSchema = new Schema({
     title: {type: String, required: true},
     description: {type: String, required: true},
-    file: {type: String, required: true},
-    author: {type: Schema.Types.ObjectId, ref: 'User', required: true}
+    book: {type: String, required: true},
+    author: {type: Schema.Types.ObjectId, ref: 'User', required: true},
+    cover: {type: String, required: true},
 })
 
 const Book = model('Book', BookSchema)
